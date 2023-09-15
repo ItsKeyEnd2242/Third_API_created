8101f0bff80a277d83a351f9620245c18ee67d3c
ItsKeyEnd2242 <pedrorumoatopo@gmail.com>
2023-09-15 18:10:56 -0300

    comentary

--- src/server.js
+++ src/server.js
@@ -7,5 +7,25 @@ const dotenv = require('dotenv').config();
 // definindo a porta do servidor
 const PORT = process.env.PORT  || 2213;
 
+//URL: localhost:2213/api
+app.get('/api', (request, response) => {
+    response.send('Olá, estou na minha api!');
+});
+
+//URL: localhost:2213/api
+app.post('/api', (request, response) => {
+    response.send('Metodo utilizado para salvar informações');
+});
+
+//URL: localhost:2213/api
+app.put('/api', (request, response) => {
+    response.send('Metodo utilizado para substituir informações');
+});
+
+app.delete('/api', (request, response) => {
+    response.send('DELETADOOOOO!');
+});
+
+
 // testar o servidor
 app.listen(PORT, () => console.log(`Running at the port ${PORT}`));
