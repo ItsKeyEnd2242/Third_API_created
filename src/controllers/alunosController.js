7df3750af60366747560c1e256fb4f97de8b9da7
ItsKeyEnd2242 <pedrorumoaotopo@gmail.com>
2023-11-10 18:04:17 -0300

    text

--- src/controllers/alunosController.js
+++ src/controllers/alunosController.js
@@ -1,9 +1,30 @@
+    // importar a conexão com o banco de dados
 const connection = require('../config/db');
 
+    // função async: realiza uma requisição de forma paralela ao sistema
+    // request: nesta variável reocupamos os dados enviados na requisição
+    // response: tem a responsabilidade de retornar as informações para aquem requisitou 
 async function listarUsuarios(request, response) {
-    const query = 'SELECT * FROM alunos;';
+    // Variável para executar a consulta no banco
 
-    connection.query(query, (err, results) => {
+async function cadastrarAluno(request, response) {
+    // passo 1: recuperar variaveis
+    // passo 2: montar query para inserir dados
+    // passo 3: tentar executar a ação no banco
+    // passo 4: definir retorno de requisição
+}
+
+    // recuperando dados da requisição
+    const params = Array (
+        request.body.nome,
+        request.body.dt_nascimento,
+        request.body.time_do_coracao
+    )
+
+    const query = 'SELECT * FROM alunos;';
+    //  err: retorna erros na execução
+    //  results: retorna a ação realizada com sucesso
+    connection.query (query, (err, results) => {
         if(results) {
             response
                 .status(200)
@@ -14,10 +35,33 @@ async function listarUsuarios(request, response) {
                 }
             );
         
-        } else {
-
+    } else {
+        response
+        .status(400)
+        .json({
+            sucess: false,
+            message: "ooops! não foi possivel listar as informações solicitadas!",
+            data: err
         }
-    });
-}
+        );
+    }})
 
-module.exports = listarUsuarios;
+    const query = 'INSERT INTO alunos(nome, dt_nascimento, time_do_coracao) values(?, ?, ?);';
+
+    connection.query(query, params, (err, results) => {
+        if (results) {
+            response
+            .status(201)
+            .json({
+                sucess: true,
+                message: "Sucesso! Aluno cadastrado",
+                data: result
+            })
+        }
+    }
+)}
+
+module.exports = {
+    listarUsuarios,
+    cadastrarAluno
+}; 
--- src/routes/alunosRouter.js
+++ src/routes/alunosRouter.js
@@ -3,7 +3,11 @@ const { Router } = require('express');
 const router = Router();
 
 const listarUsuarios = require('../controllers/alunosController');
-
+// importar funç~eos para as rotas
+const {
+    listarUsuarios,
+    cadastrarAluno
+} = require('../controllers/alunosController')
 router.get('/alunos', listarUsuarios);
-
+router.post('/aluno/create')
 module.exports = router;
